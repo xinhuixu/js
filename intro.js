342da41254e0572098f722f3ad32d347aa013c44
Xinhui <x.xu.1102@gmail.com>
2016-12-16 14:31:38 -0500

    hover func

--- intro.js
+++ intro.js
@@ -11,3 +11,22 @@ var add_element = function() {
 
 btn.addEventListener("click", add_element);
 
+/*---------------------*/
+
+var h = document.getElementById('h');
+var items = document.getElementsByTagName('li');
+var h_old = h.innerHTML;
+
+var set_heading = function() {
+    var h_content = this.innerHTML;
+    h.innerHTML = h_content;
+}
+var revert_heading = function() {
+    h.innerHTML = h_old;
+}
+for (var i=0; i<items.length; i++){
+    items[i].addEventListener('mouseover', set_heading);
+    items[i].addEventListener('mouseout', revert_heading);
+}
+ 
+
